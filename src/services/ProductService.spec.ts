75221f26e023e952f1ebfeded728300f75eb16c2
gomes-leonardo <leonardo.r.gomes@hotmail.com>
2025-11-30 18:40:04 -0300

    jest setup and first products test

---
+++ src/services/ProductService.spec.ts
@@ -0,0 +1,66 @@
+import { ProductService } from "./ProductService";
+import { AppError } from "../errors/AppError";
+import { pool } from "../db";
+
+jest.mock("../db", () => {
+  return {
+    pool: {
+      query: jest.fn(),
+    },
+  };
+});
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+const mockedPool = pool as jest.Mocked<typeof pool>;
+
+describe("ProductService", () => {
+  it("should throw an error if name or price is missing", async () => {
+    const productService = new ProductService();
+    await expect(productService.create("", 0)).rejects.toBeInstanceOf(AppError);
+  });
+  it("should throw an error if name is missing", async () => {
+    const productService = new ProductService();
+
+    await expect(productService.create("", 5000)).rejects.toBeInstanceOf(
+      AppError
+    );
+  });
+  it("should throw an error if price is missing", async () => {
+    const productService = new ProductService();
+
+    await expect(
+      productService.create("Macbook air 2010", 0)
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should throw a specific error message", async () => {
+    const productService = new ProductService();
+
+    try {
+      await productService.create("", 10);
+    } catch (error) {
+      expect(error).toBeInstanceOf(AppError);
+      expect((error as AppError).message).toBe(
+        "Nome e preços são obrigatórios."
+      );
+    }
+  });
+  it("should create a product successfully", async () => {
+    const productService = new ProductService();
+
+    (mockedPool.query as jest.Mock).mockResolvedValue({
+      rows: [{ id: 1, name: "Iphone", price: 5000 }],
+      rowCount: 1,
+    });
+
+    const result = await productService.create("Iphone", 5000);
+
+    expect(result).toHaveProperty("id", 1);
+    expect(result.name).toBe("Iphone");
+
+    expect(mockedPool.query).toHaveBeenCalledTimes(1);
+  });
+});
